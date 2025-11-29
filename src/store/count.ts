d370a32adfc3cc96d2b617746749f806faa8fa43
Byeong Min Cho <merrybmc@gmail.com>
2025-11-29 16:19:58 +0900

    zustand middleware combine, immer

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,23 +1,7 @@
 import { create } from "zustand";
-// Store를 생성하는 역할
-// 전역상태인 State와 Action 함수를 포함하는 객체
+import { combine } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
-// get
-// 현재 store 객체를 그대로 반환
-
-// set
-// 현재 store 객체의 값을 업데이트
-// zustand의 set 메서드는 인수로 전달된 프로퍼티 값만 업데이트하고
-// 명시되어있지 않은 프로퍼티는 유지시킨다.
-// 함수형 업데이트도 지원
-
-// type Store = {
-//   count: number;
-//   increase: () => void;
-//   decrease: () => void;
-// };
-
-// Selector 묶어서 불러올 때 actions
 type Store = {
   count: number;
   actions: {
@@ -26,31 +10,73 @@ type Store = {
   };
 };
 
-// 다른 컴포넌트에서 useCountStore hook을 호출 가능
-export const useCountStore = create<Store>((set, get) => {
-  return {
-    count: 0, // state
-    actions: {
-      increase: () => {
-        // Action case 1
-        // const count = get().count;
-        // set({ count: count + 1 });
+// combine이 객체 형태의 Store를 생성
+// 첫 번째 인수로 전달된 것은 state로 포함
+// 두 번쨰 인수로 전달된 콜백함수의 반환값은 actions으로 포함
+// combine 사용 이유 : combine의 첫 번째 인수로 전달된 state의 타입이 자동으로 추론됨
+// 주의할 점 : combine은 첫 번째 인수로 전달된 State 값만 포함하는 타입으로 추론한다.
+// export const useCountStore = create(
+//   combine({ count: 0 }, (set, get) => ({
+//     actions: {
+//       increase: () => {
+//         set((state) => ({
+//           count: state.count + 1,
+//         }));
+//       },
+//       decrease: () => {
+//         set((state) => ({
+//           count: state.count - 1,
+//         }));
+//       },
+//     },
+//   })),
+// );
 
-        // Action case 2 함수형 업데이트 (더 자주 사용, get을 쓸 필요 없음)
-        set((store) => ({
-          count: store.count + 1,
-        }));
+// 불변성 관리 immer
+// npm i immer
+// Store 내부의 특정 State를 업데이트할 때 직접적으로 접근해서 값을 바꾸는게 아닌
+// 변경될 값을 포함한 새로운 객체를 만들어서 전달하여 업데이트하는 방식
+// 업데이트해야할 데이터의 구조가 복잡해질경우 사용
+export const useCountStore = create(
+  immer(
+    combine({ count: 0 }, (set, get) => ({
+      actions: {
+        increase: () => {
+          set((state) => {
+            state.count += 1;
+          });
+        },
+        decrease: () => {
+          set((state) => {
+            state.count -= 1;
+          });
+        },
       },
-      decrease: () => {
-        set((store) => ({
-          count: store.count - 1,
-        }));
-      },
-    },
-  };
-});
+    })),
+  ),
+);
+
+// export const useCountStore = create<Store>((set, get) => {
+//   return {
+//     count: 0, // state
+
+//     actions: {
+//       increase: () => {
+//         set((store) => ({
+//           count: store.count + 1,
+//         }));
+//       },
+//       decrease: () => {
+//         set((store) => ({
+//           count: store.count - 1,
+//         }));
+//       },
+//     },
+//   };
+// });
 
 // custom Hook
+
 export const useCount = () => {
   const count = useCountStore((store) => store.count);
   return count;
